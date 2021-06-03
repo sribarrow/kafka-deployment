75004f2fdbed7dcc86ddb8d0a49fca5b6a64f4be
Sribarrow <sribarrow@gmail.com>
2021-06-03 18:33:50 +0100

    commented confluent start

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
 cd $CONFLUENT_HOME/bin
+echo "`pwd` - current directory" >> /tmp/kafka_deploy.log
 echo "`date` starting services..." >> /tmp/kafka_deploy.log
-confluent local start
+#confluent local start
 echo "`date` checking Status" >> /tmp/kafka_deploy.log
-confluent local status >> /tmp/kafka_deploy.log
+#confluent local status >> /tmp/kafka_deploy.log
