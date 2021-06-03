b982ca8353ca38a81a4e5cb5811647c50c98ece2
sribarrow <sribarrow@gmail.com>
2021-06-03 06:51:27 +0100

    Added logic to include if app exists

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 $INS = "echo $CONFLUENT_HOME"
-echo "`date` stopping services..." >/tmp/kafka_deploy.log
-# confluent local stop
-echo "`date` checking Status" >> /tmp/kafka_deploy.log
-# confluent local status >> /tmp/kafka_deploy.log
+if [ -z "$INS" ];
+then
+   echo "`date` stopping services..." > /tmp/kafka_deploy.log
+  # confluent local stop
+  echo "`date` checking Status" >> /tmp/kafka_deploy.log
+  # confluent local status >> /tmp/kafka_deploy.log
+
+else
+   echo "`date` New deployment..." > /tmp/kafka_deploy.log
+fi
