0efd5ed8c61bfd64a3e87317ab2763f28f4aede1
Sribarrow <sribarrow@gmail.com>
2021-06-03 18:02:52 +0100

    changed the DIR variable

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/bash
 
 #sudo yum list >> tmp/kafka_deploy.log
-sudo service codedeploy-agent status >> tmp/kafka_deploy.log
+sudo service codedeploy-agent status >> /tmp/kafka_deploy.log
 
-DIR = "/apps"
-if [ -d "${DIR}" ]; then
-    echo "`date` ${DIR} directory exists" >> /tmp/kafka_deploy.log
+sudo service codedeploy-agent status >> /tmp/kafka_deploy.log
+
+dirname  = "/apps"
+if [ -d $dirname ]; then
+    echo "`date` $dirname directory exists" >> /tmp/kafka_deploy.log
 else
-    echo "`date`  creating ${DIR} directory" >> /tmp/kafka_deploy.log
-    sudo mkdir ${DIR}
-    chmod 777 ${DIR}
+    echo "`date`  creating $dirname directory" >> /tmp/kafka_deploy.log
+    sudo mkdir $dirname
+    sudo chmod 777 $dirname
 fi
 cd /apps
+echo "pwd" >> /tmp/kafka_deploy
 echo "`date` downloading Confluent package" >> /tmp/kafka_deploy.log
 wget https://packages.confluent.io/archive/5.5/confluent-5.5.0-2.11.tar.gz
 echo "`date` untarring Confluent package" >> /tmp/kafka_deploy.log
