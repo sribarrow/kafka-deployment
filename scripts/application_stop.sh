ea1e131f1aa99d2db2b22a3e220444d7543960fd
sribarrow <sribarrow@gmail.com>
2021-06-03 06:47:52 +0100

    Update application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "`date` starting services..." >> /tmp/kafka_deploy.log
-confluent local stop
+$INS = "echo $CONFLUENT_HOME"
+echo "`date` stopping services..." >/tmp/kafka_deploy.log
+# confluent local stop
 echo "`date` checking Status" >> /tmp/kafka_deploy.log
-confluent local status >> /tmp/kafka_deploy.log
+# confluent local status >> /tmp/kafka_deploy.log
